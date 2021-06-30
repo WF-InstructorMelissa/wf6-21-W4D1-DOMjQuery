cb6dc62288d6ea3c9642b01b8a6377c51e2a658d
dojo24 <melissalongenberger24@gmail.com>
2021-06-29 21:27:31 -0400

    saving after class

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,32 @@
 $(document).ready(function() {
-
-
-
-
-    
+    $('#hide').click(function() {
+        $('nav').hide();
+    })
+    $('#show').click(function() {
+        $('nav').show();
+    })
+    $('#toggle').click(function() {
+        $('h1').animate( {
+            width: 'toggle'
+        })
+    })
+    $('#fadeOut').click(function(){
+        $('#flying').fadeOut();
+    })
+    $('#fadeIn').click(function(){
+        $('#flying').fadeIn();
+    })
+    // $('#css').click(function(){
+    //     $('#marvin').css('border', '5px groove red')
+    // })
+    $('#css').click(function(){
+        $('#marvin').css({'border': '5px groove red', 'border-radius': '20%'})
+    })
+    $('#alert').hover(function(){
+        alert('You hovered over my favorite Toon!')
+    })
+    $('#append').append(' Folks!')
 })
+// create a function that will hide the navigation
+// click event to activate the function
+// target the nav with in the function
